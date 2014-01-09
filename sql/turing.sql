48501dd115f6e653aaca92a5a7523358baddfb11
Ramon Rodrigues <cerberus.ramon@gmail.com>
2014-01-09 17:41:16 +0000

    Update

--- sql/turing.sql
+++ sql/turing.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 08, 2014 at 07:41 PM
+-- Generation Time: Jan 09, 2014 at 05:40 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
@@ -19,6 +19,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `turing`
 --
+CREATE DATABASE IF NOT EXISTS `turing` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `turing`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fix_perg`
+--
+
+CREATE TABLE IF NOT EXISTS `fix_perg` (
+  `id_fix_perg` int(11) NOT NULL AUTO_INCREMENT,
+  `fix_perg` varchar(256) NOT NULL,
+  PRIMARY KEY (`id_fix_perg`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jogo`
+--
+
+CREATE TABLE IF NOT EXISTS `jogo` (
+  `id_jogo` int(11) NOT NULL AUTO_INCREMENT,
+  `n_resp` int(11) DEFAULT NULL,
+  `ganhou` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_jogo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perguntas`
+--
+
+CREATE TABLE IF NOT EXISTS `perguntas` (
+  `id_perg` int(11) NOT NULL AUTO_INCREMENT,
+  `id_jogo` int(11) NOT NULL,
+  `id_resp` int(11) DEFAULT NULL,
+  `perg` varchar(256) NOT NULL,
+  `sala_a` int(11) NOT NULL,
+  `sala_b` int(11) NOT NULL,
+  PRIMARY KEY (`id_perg`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
 
 -- --------------------------------------------------------
 
